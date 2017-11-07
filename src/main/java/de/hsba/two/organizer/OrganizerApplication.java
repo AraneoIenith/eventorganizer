548ff4456a322f4d9bab537ddfe86e147edafac3
AraneoIenith <volatalize@outlook.com>
2017-11-07 19:19:22 +0100

    Base

---
+++ src/main/java/de/hsba/two/organizer/OrganizerApplication.java
@@ -0,0 +1,12 @@
+package de.hsba.two.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizerApplication.class, args);
+	}
+}
